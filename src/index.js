c12b294a500774221204eac52d690212bb39cfd1
arteemm <arteemmarteemm@gmail.com>
2021-08-16 16:02:12 +0300

    init: start morse-decoder task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,62 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+  let narr = expr.split('');
+  let arr = [];
+  let str = '';
+
+      x:for (let i = 0; i <= narr.length -1; i++) {
+      if (i < 10 ) {
+          str += narr[i]; 
+      } else {
+          arr.push(str);
+          narr.splice(0, 10);
+          str = '';
+          i = -1;
+          if (narr.length <= 10) {
+              str = narr.join('');
+              arr.push(str);
+              break;
+      }
+          continue x;
+      }
+   }
+
+  arr = arr.map(function(item) {
+      let r = item.indexOf('1');
+      item = item.split('').splice(r, ).join('');
+      return item;
+  })
+   let tere = '11';
+  let tochka = '10';
+  
+  arr = arr.map(function(item) {
+      let strin = '';
+      if(item === '*') {
+          return item;
+      }
+      for (let p = 1; p <= item.length - 1; p += 2) {
+          if(item[p] === '0') {
+              strin += '.';
+          } else {
+              strin += '-';
+          }
+      }
+      return item = strin;
+  })
+  let word = '';
+  for (let r = 0; r <= arr.length - 1; r++) {
+       if (arr[r] === '*') {
+              word += ' ';
+          }
+      for(let key in MORSE_TABLE) {
+         
+          if(arr[r] == key) {
+              word += MORSE_TABLE[key];
+      } 
+  }
+}
+  return word;
 }
 
 module.exports = {
